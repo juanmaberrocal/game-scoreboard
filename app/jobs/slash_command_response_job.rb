7754277125db815ddd82ad352979044e43c97733
juanmaberrocal <juanmaberrocal37@gmail.com>
2019-07-17 10:43:06 -0400

    added rescues on slash command job

--- app/jobs/slash_command_response_job.rb
+++ app/jobs/slash_command_response_job.rb
@@ -7,5 +7,31 @@ class SlashCommandResponseJob < ApplicationJob
 
     slash_command_service.fetch_response
     slash_command_service.post_response(response_url, user_id)
+  rescue InvalidSlashCommand => e
+    SlackPostRequest.new(response_url, user_id)
+                    .post(slack_error_message(e.message))
+  rescue => e
+    SlackPostRequest.new(response_url, user_id)
+                    .post(slack_error_message)
+  end
+
+  private
+
+  DEFAULT_ERROR_MESSAGE = 'Sorry, that didn\'t work. Please try again.'.freeze
+
+  def slack_error_message(message = DEFAULT_ERROR_MESSAGE)
+    {
+      blocks: [
+        { section:
+          {
+            type: 'text',
+            text: {
+              type: 'mrkdwn',
+              text: message
+            }
+          }
+        }
+      ]
+    }
   end
 end
