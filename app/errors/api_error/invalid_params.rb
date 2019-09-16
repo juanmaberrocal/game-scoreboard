3c44c91bb66832ee5c31fcdac4ccc4087b68644e
juanmaberrocal <juanmaberrocal37@gmail.com>
2019-09-16 13:46:18 -0400

    added custom exceptions to handle api errors

---
+++ app/errors/api_error/invalid_params.rb
@@ -0,0 +1,16 @@
+module ApiError
+  class InvalidParams < GeneralApiError
+    attr_reader :param, :value
+
+    def initialize(controller, action, param, value, custom_message = '')
+      @param = param
+      @value = value
+      super(controller, action, custom_message)
+    end
+
+    def message
+      super +
+      "Invalid `#{param}` value: `#{value}`."
+    end
+  end
+end
---
+++ app/errors/general_api_error.rb
@@ -0,0 +1,16 @@
+class GeneralApiError < StandardError
+  attr_reader :controller, :action,
+              :custom_message
+
+  def initialize(controller, action, custom_message = '')
+    @controller = controller
+    @action     = action
+    @custom_message = custom_message
+    super()
+  end
+
+  def message
+    "There was an error processing the request to #{controller}##{action}. "\
+    "#{custom_message}"
+  end
+end
