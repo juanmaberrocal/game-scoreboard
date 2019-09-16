7b84b1a5e8a832cfff7f48510b4ba08c930fb5a1
juanmaberrocal <juanmaberrocal37@gmail.com>
2019-09-16 17:18:34 -0400

    added rollbar log on api error response

--- app/controllers/concerns/api_error_handling.rb
+++ app/controllers/concerns/api_error_handling.rb
@@ -10,20 +10,27 @@ module ApiErrorHandling
   private
 
   def handle_general_error(exception)
+    api_rollbar(exception)
     render json: { error: exception.message },
            status: :internal_server_error,
            error: true
   end
 
   def handle_invalid_params(exception)
+    api_rollbar(exception)
     render json: { error: exception.message }, 
            status: :bad_request,
            error: true
   end
 
   def handle_unprocessable_entity(exception)
+    api_rollbar(exception)
     render json: { error: exception.message },
            status: :unprocessable_entity,
            error: true
+  end
+
+  def api_rollbar(e)
+    Rollbar.error(e, e.message, controller: params[:controller], action: params[:action])
   end
 end
