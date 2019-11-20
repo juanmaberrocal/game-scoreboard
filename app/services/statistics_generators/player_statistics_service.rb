f1f7e5c5e4fc1a4a1649c74e2c36a8c154240e42
juanmaberrocal <juanmaberrocal37@gmail.com>
2019-11-20 09:43:12 -0500

    created service to calculate player statistics

---
+++ app/services/statistics_generators/player_statistics_service.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module StatisticsGenerators
+  class PlayerStatisticsService
+    attr_reader :record
+
+    def initialize(record)
+      @record = record
+    end
+
+    def generate
+      raw_statistics.each do |raw_statistic|
+        statistics[:breakdown][raw_statistic.game_id] = {
+          played: raw_statistic.played,
+          won:    raw_statistic.won
+        }
+        statistics[:total_played] += raw_statistic.played
+        statistics[:total_won]    += raw_statistic.won
+      end
+
+      statistics
+    end
+
+    private
+
+    def statistics
+      @statistics ||= {
+        breakdown: {},
+        total_won: 0,
+        total_played: 0
+      }
+    end
+
+    def raw_statistics
+      Match.joins(:match_players)
+           .select(['matches.game_id',
+                    'COUNT(match_players.id) AS "played"',
+                    'SUM(CAST(match_players.winner AS INT)) AS "won"'])
+           .where(match_players: { player: @record })
+           .group(:game_id)
+           .order(played: :desc, won: :desc)
+    end
+  end
+end
